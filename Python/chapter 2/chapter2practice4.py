e60c1279e7c76e19ccc93fef94b5dc2e40e6abcc
Md Shafiul Islam <mdshafiulmondol02@gmail.com>
2025-01-23 17:42:49 +0600

    chapter 2

---
+++ Python/chapter 2/chapter2practice4.py
@@ -0,0 +1,11 @@
+#use comperasion opreator to find which number is grater 
+a=float(input("enter your number:" ))
+b=float(input("Enter your second number: "))
+if(a>b):
+    print("grater number is :",a)
+    print("a>b", True or False)
+
+else:
+    print("grater number is :",b)
+print("a>b False")
+
---
+++ Python/chapter 2/chapter2practice6.py
@@ -0,0 +1,3 @@
+#write a pythone progerame to find the squre of a number that is input by user
+a=float(input("Enter your number--\n"))
+print(a**2)
