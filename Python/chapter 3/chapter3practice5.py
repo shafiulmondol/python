cc5937a85e00aa4e54526cf6287545205c7501d1
Md Shafiul Islam <mdshafiulmondol02@gmail.com>
2025-01-24 09:00:00 +0600

    chapter 3

---
+++ Python/chapter 3/chapter3practice5.py
@@ -0,0 +1,5 @@
+"""Write a program to formate the following letter usinf escape sequence charecters
+string= Dear Shafiul, this piythone course is nice. Thanks!
+"""
+string= "Dear Shafiul,\n\tThis piythone course is nice.\nThanks!"
+print(string)
